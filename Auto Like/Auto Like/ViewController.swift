3e7e74d09826a30b92c41778888a7d89689dcd29
Trung Nguyen <nguyentrongtrung@outlook.com>
2023-06-16 10:39:49 +0700

    create project

---
+++ Auto Like/Auto Like/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Auto Like
+//
+//  Created by simple on 16/06/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
